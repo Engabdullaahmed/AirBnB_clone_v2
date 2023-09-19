01ab10517806e15de244685f6e9b921fad8d8340
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-09-20 01:47:32 +0300

    unfix

--- zraxt.py
+++ zraxt.py
@@ -2,63 +2,63 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///hospital_department.db')
+engine = create_engine('sqlite:///pharmacy_Section.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
 
-class Hospital(Base):
-    __tablename__ = 'hospitals'
+class Pharmacy(Base):
+    __tablename__ = 'pharmacys'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
 
-    # Establish a one-to-many relationship with Department
-    departments = relationship(
-        'Department', back_populates='hospital', cascade='all, delete-orphan')
+    # Establish a one-to-many relationship with Section
+    Sections = relationship(
+        'Section', back_populates='pharmacy', cascade='all, delete-orphan')
 
 
-class Department(Base):
-    __tablename__ = 'departments'
+class Section(Base):
+    __tablename__ = 'Sections'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
 
-    # Create a foreign key relationship to Hospital
-    hospital_id = Column(Integer, ForeignKey('hospitals.id'))
+    # Create a foreign key relationship to Pharmacy
+    pharmacy_id = Column(Integer, ForeignKey('pharmacys.id'))
 
-    # Create a reference to the Hospital object using the relationship
-    hospital = relationship('Hospital', back_populates='departments')
+    # Create a reference to the Pharmacy object using the relationship
+    pharmacy = relationship('Pharmacy', back_populates='Sections')
 
 
 Base.metadata.create_all(engine)
 
-# Create hospitals and departments
-hospital1 = Hospital(name='General Hospital')
-department1 = Department(name='Cardiology', hospital=hospital1)
-department2 = Department(name='Pediatrics', hospital=hospital1)
+# Create pharmacys and Sections
+pharmacy1 = Pharmacy(name='General Pharmacy')
+Section1 = Section(name='Cardiology', pharmacy=pharmacy1)
+Section2 = Section(name='Pediatrics', pharmacy=pharmacy1)
 
-hospital2 = Hospital(name='Children\'s Hospital')
-department3 = Department(name='Neonatology', hospital=hospital2)
+pharmacy2 = Pharmacy(name='Children\'s Pharmacy')
+Section3 = Section(name='Neonatology', pharmacy=pharmacy2)
 
-session.add(hospital1)
-session.add(department1)
-session.add(department2)
-session.add(hospital2)
-session.add(department3)
+session.add(pharmacy1)
+session.add(Section1)
+session.add(Section2)
+session.add(pharmacy2)
+session.add(Section3)
 session.commit()
 
 # Query the database
-general_hospital = session.query(Hospital).filter_by(
-    name='General Hospital').first()
-print(f'{general_hospital.name} has departments:')
-for department in general_hospital.departments:
-    print(f'- {department.name}')
+general_pharmacy = session.query(Pharmacy).filter_by(
+    name='General Pharmacy').first()
+print(f'{general_pharmacy.name} has Sections:')
+for Section in general_pharmacy.Sections:
+    print(f'- {Section.name}')
 
-# Delete a hospital and check if linked departments are deleted automatically
-session.delete(general_hospital)
+# Delete a pharmacy and check if linked Sections are deleted automatically
+session.delete(general_pharmacy)
 session.commit()
 
-departments_in_general_hospital = session.query(
-    Department).filter_by(hospital_id=general_hospital.id).all()
-if not departments_in_general_hospital:
-    print("All departments in General Hospital have been deleted.")
+Sections_in_general_pharmacy = session.query(
+    Section).filter_by(pharmacy_id=general_pharmacy.id).all()
+if not Sections_in_general_pharmacy:
+    print("All Sections in General Pharmacy have been deleted.")
