c09cbd057e2990434725fa4d0440136f84bca8d0
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-09-20 07:28:33 +0300

    fix models

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import os
 
 
 class User(BaseModel, Base):
@@ -15,11 +16,14 @@ class User(BaseModel, Base):
     """
 
     __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship('Place', back_populates='user',
-                          cascade='all, delete-orphan')
-    reviews = relationship('Review', back_populates='user',
-                           cascade='all, delete-orphan')
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+
+    if os.environ.get("HBNB_TYPE_STORAGE") == 'db':
+        places = relationship('Place', backref='user',
+                              cascade='all, delete, delete-orphan')
+    # reviews = relationship('Review', back_populates='user',
+    #                        cascade='all, delete-orphan')
